f2f71d19728d0d7aa77836f311eb601e99c23cd8
Sergey Seroukhov <sergey.seroukhov@spirent.com>
2025-03-26 22:01:00 -0700

    Added step 2 to beacons service

---
+++ samples-beacons-python/step2/src/data/version1/BeaconV1Schema.py
@@ -0,0 +1,15 @@
+from pip_services4_commons import TypeCode
+from pip_services4_data import ObjectSchema
+
+
+class BeaconV1Schema(ObjectSchema):
+    def __init__(self):
+        super(ObjectSchema, self).__init__()
+
+        self.with_optional_property("id", TypeCode.String)
+        self.with_required_property("site_id", TypeCode.String)
+        self.with_optional_property("type", TypeCode.String)
+        self.with_required_property("udi", TypeCode.String)
+        self.with_optional_property("label", TypeCode.String)
+        self.with_optional_property("center", None)
+        self.with_optional_property("radius", TypeCode.Float)
