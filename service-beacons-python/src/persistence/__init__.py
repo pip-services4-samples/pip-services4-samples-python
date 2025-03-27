bfae7e950ee32081a41f820d0906503d7c2f0743
Sergey Seroukhov <sergey.seroukhov@spirent.com>
2025-03-26 23:58:31 -0700

    Minor fix

--- service-beacons-python/src/persistence/__init__.py
+++ service-beacons-python/src/persistence/__init__.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 
-#__all__ = ['IBeaconsPersistence', 'BeaconsMongoDbPersistence', 'BeaconsMemoryPersistence', 'BeaconsFilePersistence']
-__all__ = ['IBeaconsPersistence', 'BeaconsMemoryPersistence', 'BeaconsFilePersistence']
+__all__ = ['IBeaconsPersistence', 'BeaconsMongoDbPersistence', 'BeaconsMemoryPersistence', 'BeaconsFilePersistence']
 
 from .BeaconsFilePersistence import BeaconsFilePersistence
 from .BeaconsMemoryPersistence import BeaconsMemoryPersistence
-#from .BeaconsMongoDbPersistence import BeaconsMongoDbPersistence
+from .BeaconsMongoDbPersistence import BeaconsMongoDbPersistence
 from .IBeaconsPersistence import IBeaconsPersistence
