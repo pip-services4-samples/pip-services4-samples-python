7232067bf465de066f7e00a8a14b37de742c87ae
Sergey Seroukhov <sergey.seroukhov@spirent.com>
2025-03-27 20:42:24 -0700

    Added step 6 to beacons service

---
+++ samples-beacons-python/step6/src/services/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__all__ = ['IBeaconsService', 'BeaconsService', 'BeaconsCommandSet']
+
+from .BeaconsCommandSet import BeaconsCommandSet
+from .BeaconsService import BeaconsService
+from .IBeaconsService import IBeaconsService
